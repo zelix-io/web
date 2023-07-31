de2bcde5c0ebce4e3a09d2e0e4848876e125896c
jay-epiclions <99158429+epic-jay-hong@users.noreply.github.com>
2023-07-31 11:47:51 +0900

    add seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,28 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import type { AppProps } from 'next/app';
 import { useEffect } from 'react';
+import { DefaultSeo } from 'next-seo';
+
+const DEFAULT_SEO = {
+  title: 'Zelix',
+  description: 'Zelix',
+  canonical: 'https://www.zelix.com',
+  openGraph: {
+    type: 'website',
+    locale: 'ko_KR',
+    url: 'https://www.zelix.com',
+    title: 'Zelix',
+    site_name: 'Zelix',
+    images: [
+      {
+        url: '/images/zx-logo.png',
+        width: 285,
+        height: 167,
+        alt: '이미지',
+      },
+    ],
+  },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -13,6 +35,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <DefaultSeo {...DEFAULT_SEO} />
       <Header />
       <Component {...pageProps} />
       <Footer />
