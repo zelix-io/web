a3f2e560642dc0933136edce5ed930922616e260
jay-epiclions <99158429+epic-jay-hong@users.noreply.github.com>
2023-10-11 17:16:54 +0900

    add modal

--- src/components/main/Intro.tsx
+++ src/components/main/Intro.tsx
@@ -4,6 +4,19 @@ import Typed from 'react-typed';
 interface IIntroProps {}
 
 const Intro: React.FunctionComponent<IIntroProps> = (props) => {
+  const openModal = React.useCallback(() => {
+    const modalElement = document.getElementById(
+      'my_modal_3'
+    ) as HTMLDialogElement | null;
+    if (modalElement) {
+      modalElement.showModal();
+    }
+  }, []);
+
+  React.useEffect(() => {
+    openModal();
+  }, []);
+
   return (
     <div
       className="hero min-h-screen bg-base-100 bg-intro"
@@ -43,6 +56,130 @@ const Intro: React.FunctionComponent<IIntroProps> = (props) => {
             Whitepaper
           </a>
         </div>
+      </div>
+      <div className="relative">
+        <dialog id="my_modal_3" className="modal">
+          <div
+            className="modal-box h-auto"
+            style={{
+              backgroundImage: "url('/images/modal-back.jpg')",
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center top',
+              paddingTop: '3rem', // ✕ 버튼의 크기와 여백을 고려한 상단 패딩
+            }}
+          >
+            <form method="dialog">
+              <button className="btn btn-sm btn-circle btn-white absolute top-2 right-2 z-10">
+                ✕
+              </button>
+            </form>
+
+            <div className="my-div pb-16">
+              <h3 className="font-bold text-lg">Dear ZELIX Community,</h3>
+
+              <div
+                id="text-box"
+                className="overflow-y-auto my-text-box overflow-hidden"
+              >
+                <p>
+                  <br />
+                  [Exciting Rebranding News: ONSTON is rebranding to ZELIX]
+                  <br />
+                  <br />
+                  Token Swap Details:
+                  <br />
+                  - Previous Token Name: ONSTON
+                  <br />
+                  - New Token Name: ZELIX
+                  <br />
+                  - Swap Ratio: For every 1 ONSTON token, you will receive 10
+                  ZELIX tokens.
+                  <br />
+                  - Swap Platforms: Huobi, Kucoin, Poloniex, Gate.io, Mexc
+                  <br />
+                  - Snapshot Date: October 10, 00:00 (GMT+8)
+                  <br />
+                  <br />
+                  For those who wish to personally request a swap, please use
+                  the following link: <br />
+                  <a
+                    href="https://forms.gle/96RYvuUiv4veqBGGA"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="link link-secondary"
+                  >
+                    https://forms.gle/96RYvuUiv4veqBGGA
+                  </a>
+                  <br />
+                  - Application period : 2023.10.10 00:00 (GMT+0) ~ 2023.10.16
+                  24:00 (GMT+0)
+                  <br />
+                  More Detail:
+                  <br />
+                  <a
+                    href="https://link.medium.com/sx4eaPzsyDb"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="link link-secondary"
+                  >
+                    https://link.medium.com/sx4eaPzsyDb
+                  </a>
+                  <br />
+                  [Notice on termination of ONSTON Wallet service]
+                  <br />
+                  <br />
+                  As ONSTON tokens are swapped to ZELIX, ONSTON wallet service
+                  will be terminated. (
+                  <a
+                    href="http://onston.club/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="link link-secondary"
+                  >
+                    http://onston.club/
+                  </a>
+                  )
+                  <br />
+                  ONSTON wallet service will be terminated according to the
+                  schedule below and swap to ZELIX according to application
+                  details.
+                  <br />
+                  <br />
+                  ONSTON Wallet Service Details:
+                  <br />
+                  - Wallet deposit/withdrawal suspension: 2023.10.11 00:00
+                  (GMT+9)~
+                  <br />
+                  - Wallet service terminated: 2023.10.31 24:00 (GMT+9)
+                  <br />
+                  - Services (withdrawal and swap) are not supported for assets
+                  that have not been applied for and withdrawn within the
+                  relevant period.
+                  <br />
+                  <br />
+                  For those who wish to request withdrawal and swap, please use
+                  the following link:
+                  <br />
+                  <a
+                    href=" https://forms.gle/pHinoShqENvNtC6v9"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="link link-secondary"
+                  >
+                    https://forms.gle/pHinoShqENvNtC6v9
+                  </a>
+                  <br />
+                  - Application period : 2023.10.11 00:00 (GMT+9) ~ 2023.10.24
+                  24:00 (GMT+9)
+                  <br />
+                  <br />
+                  With deep appreciation, The ZELIX Team
+                </p>
+              </div>
+            </div>
+          </div>
+        </dialog>
       </div>
     </div>
   );
