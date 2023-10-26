9a47092af9a6518591d89d50f706d562c0da46da
jay-epiclions <99158429+epic-jay-hong@users.noreply.github.com>
2023-10-26 13:39:16 +0900

    add link

--- src/components/main/Investment.tsx
+++ src/components/main/Investment.tsx
@@ -2,6 +2,14 @@ import * as React from 'react';
 
 interface IInvestmentProps {}
 
+const ExchangeLink = [
+  'https://www.kucoin.com/trade/ZELIX-USDT',
+  'https://www.mexc.com/exchange/ZELIX_USDT',
+  'https://www.htx.com/en-us/trade/zelix_usdt',
+  'https://www.gate.io/trade/ZELIX_USDT',
+  'https://www.poloniex.com/trade/ZELIX_USDT',
+];
+
 const Investment: React.FunctionComponent<IInvestmentProps> = (props) => {
   return (
     <>
@@ -9,18 +17,27 @@ const Investment: React.FunctionComponent<IInvestmentProps> = (props) => {
         className="hero bg-base-200 overflow-hidden py-20"
         style={{ backgroundImage: 'url(/images/7_investment.png' }}
       >
-        <div className="hero-content text-center flex-col sm:px-0" id='exchanges'>
+        <div
+          className="hero-content text-center flex-col sm:px-0"
+          id="exchanges"
+        >
           <div className="w-full py-20 text-center">
             <h1 className="text-3xl md:text-5xl font-bold" data-aos="fade-up">
               Investment & Market Making
             </h1>
             <div className="mt-5 flex justify-center py-10">
               <div className="btn btn-neutral w-40 h-20" data-aos="fade-up">
-                <img
-                  src="/images/dwf.png"
-                  alt="dwf"
-                  className="w-full h-auto"
-                />{' '}
+                <a
+                  className="link link-hover"
+                  href="https://www.dwf-labs.com/dwf-labs-portfolio-project/18-metaverse-gamefi/130-onston"
+                  target="_blank"
+                >
+                  <img
+                    src="/images/dwf.png"
+                    alt="dwf"
+                    className="w-full h-auto"
+                  />{' '}
+                </a>
               </div>
             </div>
             <div className="w-full pt-20 px-0">
@@ -34,12 +51,18 @@ const Investment: React.FunctionComponent<IInvestmentProps> = (props) => {
                 >
                   {Array.from({ length: 5 }).map((_, i) => (
                     <div key={`Alliance_${i}`} className="btn btn-neutral h-20">
-                      <img
-                        src={`/images/exchanges/exchange-${i + 1}.png`}
-                        alt="themepark"
-                        // className="rounded-xl"
-                        style={{ width: 140, height: 60 }}
-                      />
+                      <a
+                        className="link link-hover"
+                        href={ExchangeLink[i]}
+                        target="_blank"
+                      >
+                        <img
+                          src={`/images/exchanges/exchange-${i + 1}.png`}
+                          alt="themepark"
+                          // className="rounded-xl"
+                          style={{ width: 140, height: 60 }}
+                        />
+                      </a>
                     </div>
                   ))}
                 </div>
